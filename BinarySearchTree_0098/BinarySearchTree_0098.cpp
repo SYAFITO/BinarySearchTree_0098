51994886061f6d55b1f9734fdad016ccf01cf460
SYAFITO <Lenovo@DESKTOP-4CQ6R6H>
2024-06-14 14:26:57 +0700

    fungsi ini mencari node saat ini dari node yang ditentukan serta node saat ini dari induknya

--- BinarySearchTree_0098/BinarySearchTree_0098.cpp
+++ BinarySearchTree_0098/BinarySearchTree_0098.cpp
@@ -23,23 +23,48 @@ public:
 
 	BinaryTree()
 	{
-		ROOT = nullptr; // Initializing ROOT to NULL
+		ROOT = NULL; // Initializing ROOT to NULL
 	}
 	void insert(string element) // Insert a node in the binary search tree
 	{
-		Node* newNode = new Node(element, nullptr, nullptr); // Allocate memory for the new node
+		Node* newNode = new Node(element, NULL, NULL); // Allocate memory for the new node
 		newNode->info = element; // Assign value to the data field of the new node
-		newNode->leftchild = nullptr; // Make the left child of the new node point to NULL
-		newNode->rightchild = nullptr; // Make the right child of the new node point NULL
+		newNode->leftchild = NULL; // Make the left child of the new node point to NULL
+		newNode->rightchild = NULL; // Make the right child of the new node point NULL
 
-		Node* parent = nullptr;
-		Node* currentNode = nullptr;
+		Node* parent = NULL;
+		Node* currentNode = NULL;
+		
 		search(element, parent, currentNode); // locate the node which be the parent of the node to be inserted
 
 		if (parent == nullptr) // If the parent is NULL (Tree is empty)
 		{
 			ROOT = newNode; // Mark the new node as ROOT
 			return; // EXIT
+		}
+
+		if (element < parent->info) // if the value in the data field of the new node is less than that of the parent
+		{
+			parent->leftchild = newNode; // Make the left child of the parent point to the new node
+		}
+
+		else if (element > parent->info) // If the value in the data field of the new node is grater than that of the parent
+		{
+			parent->rightchild = newNode; // Make the right child of the parent point to the new node
+		}
+
+	}
+	void search(string element, Node*& parent, Node*& currentNode)
+	{
+		currentNode = ROOT;
+		parent = nullptr;
+		while ((currentNode != nullptr) && (currentNode->info != element))
+		{
+			parent = currentNode;
+			if (element < currentNode->info)
+				currentNode = currentNode->leftchild;
+			else
+				currentNode = currentNode->rightchild;
 		}
 	}
 };
